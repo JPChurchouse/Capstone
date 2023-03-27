053acf05a1dbd3a2449720f36df06d9a49e99d22
Sophie Banks <61633685+boringNectarine0@users.noreply.github.com>
2023-03-27 16:49:17 +1300

    Made the serial work to output text doc
    Next step adding some UI for file naming, program ending and add auto com port finding

--- serial_to_txt.py
+++ serial_to_txt.py
@@ -1,34 +1,34 @@
-import serial
+import serial  # ensure that pyserial is installed on PC and that 'serial' has been uninstalled
 import time
-import csv
-
 
 ## Serial configuration
-esp_port = serial.Serial('COM1')
+esp_port = serial.Serial(port = 'COM4', baudrate = 115200, parity = serial.PARITY_NONE, stopbits = 1, bytesize = 8)
 esp_port.flushInput()
-esp_port.baudrate = 115200
-esp_port.bytesize = 8
-esp_port.parity = serial.PARITY_NONE
-esp_port.stopbits = 1
 
 # File Configuration
 new_file = open("test.csv", "a")
-write_file = csv.writer(new_file, delimiter="   ")
+#write_file = csv.writer(new_file, delimiter = ',')
+header = "Count,Inc,Dist,Angle\n"
+#','.join(header)
+new_file.write(str(header))
 
+count   = 0
+inc     = 0
+dist    = 0
+angle   = 0
 
 while True:
     try:
         # Get Serial Input
         esp_port_bytes = esp_port.readline()
         esp_port_line = str(esp_port_bytes[0:len(esp_port_bytes)-2].decode("utf-8"))
-        esp_port_line.split() # maybe delete
         # Add to file
-        write_file.writerow([time.time(), esp_port_line])
+        if(esp_port_line[0] != '>'):
+            new_file.write(esp_port_line+'\n')
         #print(esp_port_line) # to test
     except:
         print("Keyboard Interrupt")
+        new_file.close()
         break
 # close file
-new_file.close()
-    
-        
+new_file.close()  
