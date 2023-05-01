6c0cea5b9291d098cfe3b94680cdf7121c4d11fd
Jamie Churchouse <jamespeterchurchouse@gmail.com>
2023-05-01 13:44:36 +1200

    fix 8266 lights

--- PositioningLights/PositioningLights_V00_8266/src/main.cpp
+++ PositioningLights/PositioningLights_V00_8266/src/main.cpp
@@ -2,18 +2,19 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#define NUM 8
-#define PIN 6
+#define NUM 10
+#define PIN 2
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip (NUM, PIN, NEO_GRB + NEO_KHZ800);
 uint8 c[3] {255, 0, 0};
 
 
 void setup() 
 {
   strip.begin();
-  strip.setBrightness(50);
+  //strip.setBrightness(255);
   strip.show(); // Initialize all pixels to 'off'
+  Serial.begin(115200);
 }
 
 void loop() 
@@ -22,5 +23,7 @@ void loop()
   {
     strip.setPixelColor(i, strip.Color(c[0], c[1], c[2]));
   }
-  return;
+  strip.show();
+  Serial.println("Ping");
+  delay(1000);
 }
