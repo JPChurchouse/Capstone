9feaab14b426b5e25ab5054e4bc4f495ce9ca029
JPChurchouse <116853805+JPChurchouse@users.noreply.github.com>
2023-05-16 20:11:16 +1200

    added functionality to handle the requests

--- SplitLaneTracker_Server/Program.cs
+++ SplitLaneTracker_Server/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,23 @@ namespace SplitLaneTracker_Server
 {
     internal partial class Program
     {
+        // YOU SHOULD ONLY NEED TO PLAY WITH THIS REGION:
+        #region PROCESSING
+
+        static string HandleGet(string info)
+        {
+            string thingy = "You asked for: " + info;
+            return thingy;
+        }
+
+        static void HandlePut(string info)
+        {
+
+        }
+
+        #endregion
+
+        // HOLD UP, YOU DON'T NEED TO GO ANY FURTHUR, IT SHOULD WORK FROM THIS POINT
         #region MAIN SERVER FUNCTIONALITY
         static void Main()
         {
@@ -59,9 +77,6 @@ namespace SplitLaneTracker_Server
                 listener?.Stop();
             }
         }
-        #endregion
-
-        #region PROCESSING
 
         static void ProcessClientRequest(TcpClient client)
         {
@@ -72,28 +87,35 @@ namespace SplitLaneTracker_Server
             byte[] buffer = new byte[1024];
             int bytesRead = stream.Read(buffer, 0, buffer.Length);
             string request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-            Log.Debug("Received request: {0}", request);
+            //Log.Debug("Received request: {0}", request);
 
             // Parse the request method
             string[] requestLines = request.Split('\n');
-            string requestMethod = requestLines[0].Split(' ')[0].Trim();
+            string[] requestHead = requestLines[0].Split(' ');
+            string requestMethod = requestHead[0].Trim();
+            string requestPage = requestHead[1].Trim();
+
+            Log.Debug("Requested page: " + requestPage);
 
             // Process the request based on the method
             if (requestMethod == "PUT")
             {
                 // Extract the data from the request (assuming the data is in the body)
                 int bodyIndex = Array.IndexOf(requestLines, "\r");
-                string requestData = string.Join("\n", requestLines, bodyIndex + 1, requestLines.Length - bodyIndex - 1);
-                Log.Debug("PUT request data: {0}", requestData);
+                //string requestData = string.Join("\n", requestLines, bodyIndex + 1, requestLines.Length - bodyIndex - 1);
+                //Log.Debug("PUT request data: {0}", requestData);
 
                 // TODO: Handle the PUT request as needed
+                HandlePut("ping");
             }
             else if (requestMethod == "GET")
             {
                 // TODO: Handle the GET request as needed
+                string information = HandleGet(requestPage);
 
                 // Send a response to the client
-                string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, world!";
+                string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+                response += information;
                 byte[] responseBytes = Encoding.ASCII.GetBytes(response);
                 stream.Write(responseBytes, 0, responseBytes.Length);
                 Log.Debug("Sent response: {0}", response);
