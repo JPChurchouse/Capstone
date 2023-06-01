f4b7d98a0f9367afeeb415dc3ff032814e80318b
Jamie Churchouse <jamespeterchurchouse@gmail.com>
2023-06-01 17:16:53 +1200

    WIP

---
+++ 8]_CommunicationsProtocols/FunctionTestApp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using static XkartsCommonFunctions.Colours;
+
+namespace FunctionTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                testfunc();
+
+                var steve = GetColoursList();
+                foreach (var item in steve)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+
+        }
+    }
+}
---
+++ 8]_CommunicationsProtocols/XkartsCommonFunctions/Colours.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace XkartsCommonFunctions
+{
+    public static class Colours
+    {
+        public static void testfunc()
+        {
+            Console.WriteLine("TESTING");
+        }
+        private static string file_colours = "ColoursList.json";
+        public struct Colour
+        {
+            public UInt16 Red { get; set; }
+            public UInt16 Green { get; set; }
+            public UInt16 Blue { get; set; }
+        }
+
+        public static List<Colour> GetColoursList()
+        {
+            Console.WriteLine($"File: {file_colours}");
+            string json = File.ReadAllText(file_colours);
+            Console.WriteLine($"Colours: {json}");
+            List<Colour> list = JsonConvert.DeserializeObject<List<Colour>>(json);
+            if (list == null) throw new FileNotFoundException("No list exists");
+            return list;
+        }
+    }
+}
