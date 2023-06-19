ccd3caad6287ed7f0d6ae751f49c6618b96d14ef
JPChurchouse <116853805+JPChurchouse@users.noreply.github.com>
2023-06-19 20:11:29 +1200

    SERVER: WIP

---
+++ 8]_Splitlane_Tracker_Programs/Splitlane_Tracker_Programs/ServerLibrary/HandleClient.cs
@@ -0,0 +1,74 @@
+﻿using Serilog;
+using System.Net.Sockets;
+using System.Net;
+using XKarts;
+using System.Text;
+
+namespace Server
+{
+    public partial class Server
+    {
+        static string HandleGet(string info)
+        {
+            Log.Information($"HandleGet: {info}");
+            return "You've been, " + info + " struck!!!";
+        }
+
+        static void HandlePut(string info)
+        {
+            Log.Information($"HandlePut: {info}");
+        }
+
+        static void ProcessClientRequest(TcpClient client)
+        {
+            // Get the client stream
+            NetworkStream stream = client.GetStream();
+
+            // Read the request from the client
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string request = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Log.Debug("Received request: {0}", request);
+
+            // Parse the request method
+            string[] requestLines = request.Split('\n');
+            string[] requestHead = requestLines[0].Split(' ');
+            string requestMethod = requestHead[0].Trim();
+            string requestPage = requestHead[1].Trim();
+
+            Log.Debug("Requested page: " + requestPage);
+
+            // Process the request based on the method
+            if (requestMethod == "PUT")
+            {
+                // Extract the data from the request (assuming the data is in the body)
+                int bodyIndex = Array.IndexOf(requestLines, "\r");
+                string requestData = string.Join("\n", requestLines, bodyIndex + 1, requestLines.Length - bodyIndex - 1);
+                Log.Debug("PUT request data: {0}", requestData);
+
+                // TODO: Handle the PUT request as needed
+                HandlePut(requestData);
+            }
+            else if (requestMethod == "GET")
+            {
+                // TODO: Handle the GET request as needed
+                string information = HandleGet(requestPage);
+
+                // Send a response to the client
+                string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+                response += information;
+                byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                stream.Write(responseBytes, 0, responseBytes.Length);
+                Log.Debug("Sent response: {0}", response);
+            }
+            else
+            {
+                // Unsupported method
+                string response = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
+                byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+                stream.Write(responseBytes, 0, responseBytes.Length);
+                Log.Debug("Sent response: {0}", response);
+            }
+        }
+    }
+}
---
+++ 8]_Splitlane_Tracker_Programs/Splitlane_Tracker_Programs/ServerLibrary/Main.cs
@@ -0,0 +1,64 @@
+﻿using Serilog;
+using System.Data;
+using System.Net.Sockets;
+using System.Net;
+using XKarts;
+using XKarts.Server;
+
+namespace Server
+{
+    public partial class Server
+    {
+        // Logger
+        private XKarts.Logging.Logger log = new XKarts.Logging.Logger();
+
+        // List of active Karts and their info
+        private List<KartStats> KartList = new List<KartStats>();
+
+        // Number of required laps
+        private byte Laps_Left, Laps_Right, Laps_Total;
+
+
+
+        private void ListenTCP()
+        {
+            TcpListener listener = null;
+            try
+            {
+                // Set the IP address and port number for the server
+                IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+                int port = 6969;
+
+                // Create the TCP listener
+                listener = new TcpListener(ipAddress, port);
+
+                // Start listening for client requests
+                listener.Start();
+                Log.Information("Server started. Listening on {0}:{1}", ipAddress, port);
+
+                while (true)
+                {
+                    Log.Information("Waiting for client connection...");
+                    TcpClient client = listener.AcceptTcpClient();
+                    Log.Information("Client connected!");
+
+                    // Process the client request
+                    ProcessClientRequest(client);
+
+                    // Close the client connection
+                    client.Close();
+                    Log.Information("Client connection closed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred: {0}", ex.Message);
+            }
+            finally
+            {
+                // Stop listening and clean up
+                listener?.Stop();
+            }
+        }
+    }
+}
