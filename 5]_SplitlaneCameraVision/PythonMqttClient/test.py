c3c18d024eec0dc8983eedd0630a80326dabf4ea
Jamie Churchouse <jamespeterchurchouse@gmail.com>
2023-07-01 18:47:18 +1200

    update py

--- 5]_SplitlaneCameraVision/PythonMqttClient/test.py
+++ 5]_SplitlaneCameraVision/PythonMqttClient/test.py
@@ -7,17 +7,17 @@ import time
 
 
 # Global vars
-broker_address = "10.2.245.108"     # MQTT broker address - will be "192.168.1.30" on site
+broker_address = "192.168.137.111"     # MQTT broker address - will be "192.168.1.30" on site
 running = False                     # Bool to tell if the race is running
 
 
 # Callback func for when client connects
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.will_set("status/detection","offline",1,True)    # Set will
+    client.will_set("status/detection","Offline",1,True)    # Set will
     client.subscribe("command/detection")                   # Sub to detection commands
     #client.subscribe("#")
-    publish_status("online")
+    publish_status("Online")
 
 # Callback func for when a message is RXD
 def on_message(client, userdata, msg):
@@ -34,6 +34,11 @@ def publish_detect(colour,lane):
     timenow = str(TimeNowMs())
     json = "{\"Time\": " + timenow + ",\"Colour\": \"" + colour + "\",\"Lane\": \"" + lane + "\"}"
     client.publish("detect", json, 1, False)
+    
+# Disconnect the client
+def Disconnect():
+	publish_status("Offline")
+	client.disconnect()
 
 # Func to process new commands for this device
 def process_command(command):
@@ -58,9 +63,12 @@ client.connect(broker_address, 1883, 60)
 
 # Vars for testing
 time_start = TimeNowMs()
-lapmsg_cols = ["red","green","red","blue","green","green","red","red","blue","green"]
+lapmsg_cols = ["red","green","yellow","red","yellow","blue","green","green","red","red","blue","green"]
 lapmsg_lane = ["left","right","right","right","left","left","right","right","right","left"]
 time_count = 0
+
+client.publish("raceinfo","{\"KartList\": [{\"Colour\": \"red\",\"Number\": 17},{\"Colour\": \"green\",\"Number\": 23},{\"Colour\": \"blue\",\"Number\": 9}],\"RequiredLaps\": [8,8,20]}",1)
+client.publish("command/race","start",1)
 
 # Main While loop
 while True:
@@ -68,7 +76,7 @@ while True:
     
     timenow = TimeNowMs()
     #print(timenow)
-    if timenow > (time_start + 2):
+    if timenow > (time_start + 500):
         time_start = timenow
         
         publish_detect(lapmsg_cols[time_count],lapmsg_lane[time_count])
@@ -76,5 +84,5 @@ while True:
         
         if time_count >= 10:
             client.publish("command/race","end")
-            #client.disconnect()
+            Disconnect()
             exit(0)
