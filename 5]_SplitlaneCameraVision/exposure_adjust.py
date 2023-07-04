a597413358b368022f220fd2c65f4b5b67938759
Sophie Banks <61633685+boringNectarine0@users.noreply.github.com>
2023-07-04 20:04:51 +1200

    more changes

--- 5]_SplitlaneCameraVision/exposure_adjust.py
+++ 5]_SplitlaneCameraVision/exposure_adjust.py
@@ -6,45 +6,50 @@ import numpy as np
 import argparse
 import subprocess
 
+#              exposure_absolute (int)    : min=3 max=2047 step=1 default=250 value=333
 
 def main():
+    exposures = [3, 10, 20, 30, 40, 50, 75, 100, 150, 200, 250]
 
-    #code
-    path = os.getcwd() + "/video_trial.avi"
-    camera = cv.VideoCapture(0)
-    camera.set(cv.CAP_PROP_AUTOFOCUS, 100)
-    if (not camera.isOpened()):
-        print(f"Error:Camera not found!")
-        return 1
-    
-    exposure = -5
-
-    while True:
-        ret, frame = camera.read()
-
-        if not ret:
-            raise IOError("Error with webcam")
+    for exposure in exposures:
+        #code
+        path = os.getcwd() + f"/exposure_{str(exposure)}.avi"
         
-        frame = cv.putText(frame, f"exposure = {exposure}", (50,50), cv.FONT_HERSHEY_SIMPLEX, 
-                        1, (200, 50, 50), 2, cv.LINE_AA)     
-                        ##start coords ##end coords
+        #or depending on camera
+        subprocess.check_call(f"v4l2-ctl -d /dev/video0 -c exposure_absolute={str(exposure)}", shell = True)
 
-        cv.imshow("frame", frame)
-        #output.write(frame)
-
-        if cv.waitKey(1) & 0xFF == ord('u'):
-            exposure = exposure + 1
-            camera.set(cv.CAP_PROP_AUTO_EXPOSURE, exposure)
-            #or depending on camera
-            #subprocess.check_call("v4l2-ctl -d /dev/video0 -c exposure_absolute=40",shell=True)
-
-        if cv.waitKey(1) & 0xFF == ord('d'):
-            exposure = exposure - 1
-            camera.set(cv.CAP_PROP_AUTO_EXPOSURE, exposure)
-
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+        camera = cv.VideoCapture(0)
+        if (not camera.isOpened()):
+            print(f"Error:Camera not found!")
+            return 1
         
+        frame_width = int(camera.get(3))
+        frame_height = int(camera.get(4))
+        frame_size = (frame_width,frame_height)
+        fps = 30
+        # write video file
+        output = cv.VideoWriter(path, cv.VideoWriter_fourcc('M','J','P','G'), fps, frame_size)
+
+        #frame counter
+        frame_num = 0
+        while frame_num < 40:
+            ret, frame = camera.read()
+
+            if not ret:
+                raise IOError("Error with webcam")
+            
+            frame = cv.putText(frame, f"exposure = {exposure}", (50,50), cv.FONT_HERSHEY_SIMPLEX, 
+                            1, (200, 50, 50), 2, cv.LINE_AA)     
+                            ##start coords ##end coords
+
+            cv.imshow("frame", frame)
+            output.write(frame)
+
+
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                    break
+        
+        camera.release()
 
     return 0
 
