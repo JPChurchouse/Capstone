97c42e6d38a0444e37839da45924133caad7957c
JPChurchouse <116853805+JPChurchouse@users.noreply.github.com>
2023-07-26 22:46:01 +1200

    transponder and station code wokring

--- 9]_UWB_Modules/Station/src/main.cpp
+++ 9]_UWB_Modules/Station/src/main.cpp
@@ -1,22 +1,38 @@
 #include <Arduino.h>
-//////
-
 #include <SPI.h>
 #include "DW1000Ranging.h"     //https://github.com/thotro/arduino-dw1000
- 
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+
 #define ANCHOR_ADD "83:17:5B:D5:A9:9A:E2:9C"
  
 #define SPI_SCK 18
 #define SPI_MISO 19
 #define SPI_MOSI 23
 #define DW_CS 4
+
+
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "Lodge Wireless Internet";
+const char* password = "JulietCharlieHotelQuebec";
+const char* mqtt_server = "192.168.1.20";
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+void setup_wifi();
+void callback(char*, byte *, unsigned int);
+void reconnect() ;
+
  
 // connection pins
 const uint8_t PIN_RST = 27; // reset pin
 const uint8_t PIN_IRQ = 34; // irq pin
 const uint8_t PIN_SS = 4;   // spi select pin
  
-
 void newRange();
 void newBlink(DW1000Device *device);
 void inactiveDevice(DW1000Device *device);
@@ -25,6 +41,12 @@ void inactiveDevice(DW1000Device *device);
 void setup()
 {
     Serial.begin(115200);
+
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
+
+
     delay(1000);
     //init the configuration
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
@@ -41,21 +63,107 @@ void setup()
     //we start the module as an anchor
     // DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
  
-    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+    //DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
     // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_LOWPOWER);
     // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_LOWPOWER);
-    // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
+    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
     // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY);
     // DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+
+    client.publish("init", "hello");
 }
  
 void loop()
 {
-    DW1000Ranging.loop();
+  DW1000Ranging.loop();
+
+  if (!client.connected()) reconnect();
+  client.loop();
+}
+
+void setup_wifi() 
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) 
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) 
+  {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "command/dock") 
+  {
+  }
+}
+
+void reconnect() 
+{
+  // Loop until we're reconnected
+  while (!client.connected()) 
+  {
+    Serial.print("Attempting MQTT connection...");
+
+    // Attempt to connect
+    if (client.connect("Dock_Client")) 
+    {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("command/dock");
+    } 
+
+    else 
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
  
 void newRange()
 {
+    char message[32];
+
+    uint16_t n = DW1000Ranging.getDistantDevice()->getShortAddress();
+    float r = DW1000Ranging.getDistantDevice()->getRange();
+
+    sprintf(message, "{\"ID\":\"%X\",\"Dist\":%f}",n,r);
+
+    client.publish("detect", message);
+
+    return;
     Serial.print("from: ");
     Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
     Serial.print("\t Range: ");
@@ -68,6 +176,7 @@ void newRange()
  
 void newBlink(DW1000Device *device)
 {
+    return;
     Serial.print("blink; 1 device added ! -> ");
     Serial.print(" short:");
     Serial.println(device->getShortAddress(), HEX);
@@ -75,6 +184,7 @@ void newBlink(DW1000Device *device)
  
 void inactiveDevice(DW1000Device *device)
 {
+    return;
     Serial.print("delete inactive device: ");
     Serial.println(device->getShortAddress(), HEX);
 }
--- 9]_UWB_Modules/Transponder/src/main.cpp
+++ 9]_UWB_Modules/Transponder/src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
-//////
-
 #include <SPI.h>
-#include "DW1000Ranging.h"
+#include <DW1000Ranging.h>
+
+// Transponder code
+char* MY_ID = "7D:00:22:EA:82:60:3B:9C";
  
 #define SPI_SCK 18
 #define SPI_MISO 19
@@ -28,16 +29,16 @@ void setup()
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
     DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
     //define the sketch as anchor. It will be great to dynamically change the type of module
-    DW1000Ranging.attachNewRange(newRange);
-    DW1000Ranging.attachNewDevice(newDevice);
-    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+    //DW1000Ranging.attachNewRange(newRange);
+    //DW1000Ranging.attachNewDevice(newDevice);
+    //DW1000Ranging.attachInactiveDevice(inactiveDevice);
     //Enable the filter to smooth the distance
     //DW1000Ranging.useRangeFilter(true);
 
     DW1000.setAntennaDelay(16438);
  
     //we start the module as a tag
-    DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+    DW1000Ranging.startAsTag(MY_ID, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
 }
  
 void loop()
@@ -47,6 +48,7 @@ void loop()
  
 void newRange()
 {
+    return;
     Serial.print("from: ");
     Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
     Serial.print("\t Range: ");
@@ -59,6 +61,7 @@ void newRange()
  
 void newDevice(DW1000Device *device)
 {
+    return;
     Serial.print("ranging init; 1 device added ! -> ");
     Serial.print(" short:");
     Serial.println(device->getShortAddress(), HEX);
@@ -66,6 +69,7 @@ void newDevice(DW1000Device *device)
  
 void inactiveDevice(DW1000Device *device)
 {
+    return;
     Serial.print("delete inactive device: ");
     Serial.println(device->getShortAddress(), HEX);
 }
